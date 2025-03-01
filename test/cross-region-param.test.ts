2fd56274a606843ed5ef9e970df24d3914fddbdf
Rudger Gravestein <r.gravestein@triple-networks.com>
2025-03-01 17:41:20 +0100

    Initial commit

---
+++ test/cross-region-param.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CrossRegionParam from '../lib/cross-region-param-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cross-region-param-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CrossRegionParam.CrossRegionParamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
