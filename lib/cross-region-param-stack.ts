b3b4048a34f291f2960d0281d957c276c23da707
Rudger Gravestein <r.gravestein@triple-networks.com>
2025-03-02 13:38:59 +0100

    Add customResource & queue properties to stack

--- lib/cross-region-param-stack.ts
+++ lib/cross-region-param-stack.ts
@@ -7,17 +7,32 @@ import { regionEU } from "../config/constants";
 export interface StackProps extends cdk.StackProps {
   env: cdk.Environment;
   crossRegionReferences?: boolean;
+  crArn?: cdk.Reference;
 }
 
 export class CrossRegionParamStack extends cdk.Stack {
+  readonly queue: sqs.Queue;
+  readonly customResource: cdk.CfnResource;
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
     if (props.env.region === regionEU) {
       const queueId = `CrossRegionParamQueue-${props.env.region}`;
       // example resource
-      const queue = new sqs.Queue(this, queueId, {
+      this.queue = new sqs.Queue(this, queueId, {
         visibilityTimeout: cdk.Duration.seconds(300)
+      });
+
+      this.customResource = new cdk.CfnResource(this, `MyResource-${props.env.region}`, {
+        type: 'Custom::MyResource',
+      });
+    } else {
+      this.customResource = new cdk.CfnResource(this, `MyResource-${props.env.region}`, {
+        type: 'Custom::MyResource',
+        properties: {
+          Prop: props.crArn,
+        },
       });
     }
   }
